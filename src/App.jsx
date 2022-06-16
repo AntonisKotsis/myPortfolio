40bc8e60f091225006a7f728156fba94deca20f0
AK47 <ak47@pop-os.localdomain>
2022-06-16 17:22:08 +0300

    Stage 1: Initializing components

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import About from './components/about_component/About'
+import Contact from './components/contact_component/Contact'
+import Experience from './components/experience_component/Experience'
+import Header from './components/header_component/Header'
+import Nav from './components/nav_component/Nav'
+import Services  from './components/services_component/Services'
+import Portfolio from './components/portfolio_component/Portfolio'
+import Footer from './components/footer_component/Footer'
+
+const App = () => {
+  return (
+    <div>
+
+      <Header/>
+      <Nav/>
+      <About/>
+      <Experience/>
+      <Services/>
+      <Portfolio/>
+      <Contact/>
+      <Footer/>
+
+
+    </div>
+  )
+}
+
+export default App;
