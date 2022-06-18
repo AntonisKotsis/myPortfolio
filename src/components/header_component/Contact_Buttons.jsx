3c2b94795b7a0e834627e9570f8f8f38a51bcfa1
AK47 <ak47@pop-os.localdomain>
2022-06-18 09:25:11 +0300

    Stage 1: General css initialization

---
+++ src/components/header_component/Contact_Buttons.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CV from '../../assets/algorithms_certificate.pdf'
+function Contact_Buttons() {
+  return (
+    <div className='contact_buttons'>
+
+
+    </div>
+  )
+}
+
+export default Contact_Buttons
--- src/components/header_component/Header.jsx
+++ src/components/header_component/Header.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import './header.css'
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='container'>
+      <h1>I am Antonis Kotsis</h1>
+      <h5 className='text-light'>a Software Engineer</h5>
+
+    </div>
   )
 }
 
