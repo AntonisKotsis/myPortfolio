b3d5fdc90e8fafa3480cfb65ba1342dc891df437
AK47 <ak47@pop-os.localdomain>
2022-06-19 20:12:37 +0300

    Stage 5: Implemented css for the nav bar component

--- src/components/nav_component/Nav.jsx
+++ src/components/nav_component/Nav.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import {MdOutlineHomeRepairService} from 'react-icons/md'
+import {BiMessageDetail} from 'react-icons/bi'
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" className='active'><AiOutlineHome/></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><BiBook/></a>
+      <a href="#portfolio"><MdOutlineHomeRepairService/></a>
+      <a href="#contact"><BiMessageDetail/></a>
+    </nav>
   )
 }
 
