99cb4c73b9d7c03e668e8b0d9408615355e250ed
AK47 <ak47@pop-os.localdomain>
2022-06-25 21:18:38 +0300

    Stage 9: Implemented Portfolio section

--- src/components/portfolio_component/Portfolio.jsx
+++ src/components/portfolio_component/Portfolio.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
 import './portfolio.css'
+import Projects from './projects'
+
 const Portfolio = () => {
+
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio_container'>
+        {
+        Projects.map(({id,image,title,github})=>{
+          return(
+            <article key={id} className='portfolio_item'>
+              <div className='portfolio_item_image'>
+                <img src={image} alt="project" />
+              </div>
+              <h3>{title}</h3>
+              <div className='portfolio_item_cta'>
+                <a href={github} className='btn btn-primary' target='_blank' >Github Repo</a>
+              </div>
+            </article>
+          )
+        })
+
+}
+      </div>
+
+    </section>
   )
 }
 
---
+++ src/components/portfolio_component/projects.js
@@ -0,0 +1,49 @@
+
+import IMG1 from '../../assets/sample1.webp'
+import IMG2 from '../../assets/sample1.webp'
+import IMG3 from '../../assets/sample1.webp'
+import IMG4 from '../../assets/sample1.webp'
+import IMG5 from '../../assets/sample1.webp'
+import IMG6 from '../../assets/sample1.webp'
+const projects=[
+    {
+        id:1,
+        image:IMG1,
+        title:"Quantifing Controversy in Covid-19",
+        github:"http://github.com"
+    },
+    {
+        id:2,
+        image:IMG2,
+        title:"Portable Order System",
+        github:"http://github.com"
+    },
+    {
+        id:3,
+        image:IMG3,
+        title:"Supreme Buy Bot",
+        github:"http://github.com"
+    },
+    {
+        id:4,
+        image:IMG4,
+        title:"Text To Speech Editor",
+        github:"http://github.com"
+    },
+    {
+        id:5,
+        image:IMG5,
+        title:"Wiki Scrapping Bot",
+        github:"http://github.com"
+    },
+    {
+        id:6,
+        image:IMG6,
+        title:"Black Jack",
+        github:"http://github.com"
+    },
+
+
+];
+
+export default projects;
